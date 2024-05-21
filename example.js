671c5585edf6d164783fdbcfc29a0c611872e8d3
Soim <soim.kim@lge.com>
2024-05-21 14:24:54 +0900

    test

---
+++ example.js
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: Copyright 2022 LG Electronics Inc.
+// SPDX-License-Identifier: LicenseRef-SOI
+
+function printHello(){
+    console.log("Hello world");
+}
+
+module.exports.Hello = printHello;
---
+++ sample.js
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: Copyright 2022 LG Electronics Inc.
+// SPDX-License-Identifier: UNKNOWN
+
+function printHello(){
+    console.log("Hello world");
+}
+
+module.exports.Hello = printHello;
